648b27712887d45ba08fb6bd08f67d43ada1ca0e
Farhan Hai Khan <njrfarhandasilva10@gmail.com>
2021-03-12 18:30:04 +0530

    add create_dir

--- common_utils.py
+++ common_utils.py
@@ -6,17 +6,19 @@ Github : @khanfarhan10
 Original File at : https://github.com/khanfarhan10/custom_utils/
 """
 
-def create_dir(dir,v = 1):
-  """Creates a directory without throwing an error if directory already exists.
-  dir : The directory to be created.
-  v : Verbosity"""
-  if not os.path.exists(dir):
-    os.makedirs(dir)
-    if v:
-      print("Created Directory : ", dir)
-    return 1
-  else:
-    if v:
-      print("Directory already existed : ", dir)
-    return 0
-  
+
+def create_dir(dir, v=1):
+    """
+    Creates a directory without throwing an error if directory already exists.
+    dir : The directory to be created.
+    v : Verbosity
+    """
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+        if v:
+            print("Created Directory : ", dir)
+        return 1
+    else:
+        if v:
+            print("Directory already existed : ", dir)
+        return 0
