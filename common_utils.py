2e49af20a6e8b08b189b839a5f4aae694de8c7e5
Farhan Hai Khan <njrfarhandasilva10@gmail.com>
2021-03-12 18:29:01 +0530

    Update common_utils.py

--- common_utils.py
+++ common_utils.py
@@ -1,3 +1,11 @@
+"""
+Common Utilities used in Day to Day Programming.
+A Python Programmers Bread and Butter.
+Author : Farhan Hai Khan
+Github : @khanfarhan10
+Original File at : https://github.com/khanfarhan10/custom_utils/
+"""
+
 def create_dir(dir,v = 1):
   """Creates a directory without throwing an error if directory already exists.
   dir : The directory to be created.
