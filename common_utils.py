f3ec884ed977a83216102dd17e8e6d0b39f09572
Farhan Hai Khan <njrfarhandasilva10@gmail.com>
2021-10-27 21:46:27 +0530

    Update common_utils.py

--- common_utils.py
+++ common_utils.py
@@ -5,6 +5,42 @@ Author : Farhan Hai Khan
 Github : @khanfarhan10
 Original File at : https://github.com/khanfarhan10/custom_utils/
 """
+import os
+def get_size(path = os.getcwd(), v= 0):
+    """Get the Size of Folder/File, v = verbosity"""
+    if v:
+        print("Calculating Size: ",path)
+    total_size = 0
+    #if path is directory--
+    if os.path.isdir(path):
+      if v:
+        print("Path type : Directory/Folder")
+      for dirpath, dirnames, filenames in os.walk(path):
+          for f in filenames:
+              fp = os.path.join(dirpath, f)
+              # skip if it is symbolic link
+              if not os.path.islink(fp):
+                  total_size += os.path.getsize(fp)
+    #if path is a file---
+    elif os.path.isfile(path):
+      if v:
+        print("Path type : File")
+      total_size=os.path.getsize(path)
+    else:
+      if v:
+        print("Path Type : Special File (Socket, FIFO, Device File)" )
+      total_size=0
+    bytesize=total_size
+    if v:
+        print(bytesize, 'bytes')
+        print(bytesize/(1024), 'kilobytes')
+        print(bytesize/(1024*1024), 'megabytes')
+        print(bytesize/(1024*1024*1024), 'gegabytes')
+    return total_size
+
+
+x=get_size("/content/examples")
+
 def props(img,show_uniques=True):
     print("Shape :",img.shape,"Maximum :",img.max(),"Minimum :",img.min(),"Data Type :",img.dtype)
     if show_uniques:
